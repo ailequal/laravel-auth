997f47e932fce2047606423e80ad739d166feb10
ailequal <37016865+ailequal@users.noreply.github.com>
2020-03-30 16:59:19 +0200

    Added comment model migration seeder

---
+++ database/migrations/2020_03_30_145329_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')
+               ->constrained();
+            $table->char('name', 100);
+            $table->char('email', 100);
+            $table->longText('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
---
+++ database/seeds/CommentSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Comment;
+
+class CommentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 24; $i++) { 
+            $comment = new Comment;
+            if ($i < 6) {
+                $comment->post_id = 1;
+            } elseif ($i < 12) {
+                $comment->post_id = 2;
+            } elseif ($i < 18) {
+                $comment->post_id = 3;
+            } else {
+                $comment->post_id = 4;
+            }
+            $comment->name = $faker->firstName() . ' ' . $faker->lastName();
+            $comment->email = $faker->email();
+            $post->text = $faker->paragraph($nbSentences = 2, $variableNbSentences = true);
+            $comment->save();
+        }
+    }
+}
