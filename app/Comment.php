452bf3a27742860c04b44b1694a1cab2c839a3e8
ailequal <37016865+ailequal@users.noreply.github.com>
2020-03-30 22:18:26 +0200

    Minor fixes

---
+++ app/Comment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'text',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function post() {
+        return $this->belongsTo('App\Post');
+    } 
+}
