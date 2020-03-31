6c7493ca28f5f7cd9a2451abae3ef475ed969585
ailequal <37016865+ailequal@users.noreply.github.com>
2020-03-31 17:07:12 +0200

    Minor visual fixes

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -5,14 +5,26 @@
 	<h1>Create view for Admin</h1>
 </div>
 <div class="container">
-	<form action="{{route('admin.posts.store')}}" method="POST">
-		@csrf
-		@method('POST')
-		<label for="title">Title</label>
-		<input type="text" name="title" placeholder="Title">
-		<label for="text">Text</label>
-		<input type="text" name="text" placeholder="Insert here your text">
-		<input type="submit" value="Submit">
-	</form>
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="card">
+				<div class="card-body">
+					<form action="{{route('admin.posts.store')}}" method="POST">
+						@csrf
+						@method('POST')
+						<div class="d-block">
+							<label for="title">Title</label>
+							<input type="text" name="title" placeholder="Title">
+						</div>
+						<div class="d-block">
+							<label for="text">Text</label>
+							<input type="text" name="text" placeholder="Insert here your text">
+						</div>
+						<input type="submit" value="Submit">
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -5,14 +5,26 @@
 	<h1>Edit view for Admin</h1>
 </div>
 <div class="container">
-	<form action="{{route('admin.posts.update', $post->id)}}" method="POST">
-		@csrf
-		@method('PATCH')
-		<label for="title">Title</label>
-		<input type="text" name="title" placeholder="Title" value="{{$post->title}}">
-		<label for="text">Text</label>
-		<input type="text" name="text" placeholder="Insert here your text" value="{{$post->text}}">
-		<input type="submit" value="Submit">
-	</form>
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="card">
+				<div class="card-body">
+					<form action="{{route('admin.posts.update', $post->id)}}" method="POST">
+						@csrf
+						@method('PATCH')
+						<div class="d-block">
+							<label for="title">Title</label>
+							<input type="text" name="title" placeholder="Title" value="{{$post->title}}">
+						</div>
+						<div class="d-block">
+							<label for="text">Text</label>
+							<input type="text" name="text" placeholder="Insert here your text" value="{{$post->text}}">
+						</div>
+						<input type="submit" value="Submit">
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
